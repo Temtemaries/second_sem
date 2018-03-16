861a1f1e764ac8cf5f3e07042a3f906a87111a12
Temtemaries <i_skater@mail.ru>
2018-03-16 22:39:01 +0300

    4

---
+++ meme/meme/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  meme
+//
+//  Created by Артём Олев on 16.03.2018.
+//  Copyright © 2018 Артём Олев. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int sum;
+    /*podshitat' rezultat*/
+    sum=25+37-19;
+    /*otobrazit' rezultat*/
+       printf("The answer is %d", sum);
+    return 0;
+}
