0fca360c4de6a933002cc2feba106a108439d767
Temtemaries <i_skater@mail.ru>
2018-03-16 22:33:01 +0300

    3

---
+++ Proga2/Proga2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Proga2
+//
+//  Created by Артём Олев on 16.03.2018.
+//  Copyright © 2018 Артём Олев. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int c=87;
+    int b=15;
+    int a;
+    a=c-b;
+    printf("a=%d", a);
+    return 0;
+}
