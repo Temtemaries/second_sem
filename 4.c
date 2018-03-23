8654e9fc829da5a6b28f71ce81c18bbb7bed07d5
Temtemaries <i_skater@mail.ru>
2018-03-23 23:48:35 +0300

    4

---
+++ 4.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  meme45
+//
+//  Created by Артём Олев on 23.03.2018.
+//  Copyright © 2018 Артём Олев. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    const int F=27;
+    float C;
+    C=(F-32)/1.8;
+    printf("Temperatura ne v burgerlandii=%f",C);
+    return 0;
+}
